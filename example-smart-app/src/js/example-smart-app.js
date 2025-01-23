e79ad77f3f0724cc530d57f58f1e3dda74049960
sschwart1234 <163345652+sschwart1234@users.noreply.github.com>
2025-01-23 15:36:41 -0500

    Update example-smart-app.js

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -27,7 +27,7 @@
         $.when(pt, obv).done(function(patient, obv) {
           var byCodes = smart.byCodes(obv, 'code');
           var gender = patient.gender;
-          var married = patient.maritalStatus;
+          var maritalStatus = patient.maritalStatus;
 
           var fname = '';
           var lname = '';
@@ -48,7 +48,7 @@
           p.gender = gender;
           p.fname = fname;
           p.lname = lname;
-          p.married = maritalStatus;
+          p.maritalStatus = maritalStatus;
           p.height = getQuantityValueAndUnit(height[0]);
 
           if (typeof systolicbp != 'undefined')  {
